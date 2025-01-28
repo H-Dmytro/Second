8cbad6160d7d7101ee2186d4d95902cf0db11e81
H-Dmytro <d.m.herasymchuk@gmail.com>
2025-01-28 11:11:17 +0200

    Added h1 tag

---
+++ test_vars_js/test_js_vars.js
@@ -0,0 +1,10 @@
+function playWithVars() {
+    const a =2;
+    let d='rrcb';
+    console.log(a,d);
+
+    const booleanVar = true;
+    const undefineVar = undefined;
+    console.log(booleanVar, undefineVar);
+}
+playWithVars();
