10971181387d49f5584460d44c974c7cca846d8a
H-Dmytro <d.m.herasymchuk@gmail.com>
2025-02-02 19:45:32 +0200

    Fixed according to eslim

--- Home_work_Lesson_3/switch.js
+++ Home_work_Lesson_3/switch.js
@@ -1,28 +1,28 @@
 function accessByName() {
-    let name = "Infna";
-    let age = 19 ;
+    const name = "Infna";
+    const age = 19 ;
     if (age < 18) {
-            switch (name){
-                case "Dima" :
-                    console.log ("Go to do home work "+ name + "."); 
+        switch (name){
+            case "Dima" :
+                console.log ("Go to do home work "+ name + ".");
                 break;
-                case "Alex" :
-                    console.log ("Go to do home work " + name + "." ); 
-                break;    
-            default :
-                console.log ("Hello guest.");
-            }
-        } else if (age >= 18) {
-            switch (name){
-                case "Inna" :
-                    console.log ("Hello "+ name + "." ); 
-                break;
-                case "Yehor" :
-                    console.log ("Hello "+ name + "." ); 
+            case "Alex" :
+                console.log ("Go to do home work " + name + "." );
                 break;
             default :
                 console.log ("Hello guest.");
-            };
+        }
+    } else if (age >= 18) {
+        switch (name){
+            case "Inna" :
+                console.log ("Hello "+ name + "." );
+                break;
+            case "Yehor" :
+                console.log ("Hello "+ name + "." );
+                break;
+            default :
+                console.log ("Hello guest.");
+        };
     }
 }
-    accessByName()
+accessByName();
