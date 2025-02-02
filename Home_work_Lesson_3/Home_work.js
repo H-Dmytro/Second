a8fb2117597f0e0ee4e5f3d0a7c8f62b8b8642d4
H-Dmytro <d.m.herasymchuk@gmail.com>
2025-02-02 16:31:58 +0200

    Added Home work 3

---
+++ Home_work_Lesson_3/Home_work.js
@@ -0,0 +1,62 @@
+function tasksWithVars() {
+    const a = 10;
+    const b = 2;
+    result_plus = a + b;
+    result_multiplication = a * b;
+    result_divison = a / b;
+    result_substraction = a - b;
+
+    console.log(result_plus, result_divison, result_multiplication, result_substraction)
+}
+function variables() { 
+    let str1 = `Hellow ` ;
+    let str2 = `World` ;
+    console.log(str1 + str2)
+}
+function logicalType() {
+
+    const booleanVar = false;
+    const undefinedVar = undefined;
+
+    console.log (booleanVar, undefinedVar)
+}
+
+function changesType() {
+
+    const change1 = `12`;
+    let change2 = Number(change1);
+
+    console.log (change2)
+}
+
+function arithmeticalOperators() {
+
+    var num = 47;
+    num += 7;
+    num -= 18;
+    num *= 10;
+    num /= 15;
+    var num2 = num;
+
+
+    console.log (num, num2++, num2++,num2--,num2)
+}
+function ifOperators() {
+
+    var num = 47;
+    num += 7;
+    num -= 18;
+    num *= 10;
+    num /= 15;
+    var num2 = num;
+
+
+    console.log (num, num2++, num2++,num2--,num2)
+}
+
+
+tasksWithVars();
+variables();
+logicalType();
+changesType()
+arithmeticalOperators()
---
+++ Home_work_Lesson_3/decision-tree.js
@@ -0,0 +1,11 @@
+function ifOperation() {
+    const a = 10;
+    let b = 2;
+    if ( a == 0 ) {console.log("Correct");
+
+    } else if ( a <= 0 ) { console.log ("The value should be more than 0");
+} else { console.log ("Incorrect");
+
+}
+}
+ifOperation()
