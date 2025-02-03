781e08d27da9b7560edfb296c6d5c93546943026
H-Dmytro <d.m.herasymchuk@gmail.com>
2025-02-03 19:24:47 +0200

    Homework 3

---
+++ Homework_3/switch.js
@@ -0,0 +1,29 @@
+function accessByName() {
+    const name = "Infna";
+    const age = 19 ;
+    if (age < 18) {
+        switch (name){
+            case "Dima" :
+                console.log ("Go to do home work "+ name + ".");
+                break;
+            case "Alex" :
+                console.log ("Go to do home work " + name + "." );
+                break;
+            default :
+                console.log ("Hello guest.");
+        }
+    } else if (age >= 18) {
+        switch (name){
+            case "Inna" :
+                console.log ("Hello "+ name + "." );
+                break;
+            case "Yehor" :
+                console.log ("Hello "+ name + "." );
+                break;
+            default :
+                console.log ("Hello guest.");
+        };
+    }
+}
+
+accessByName();
