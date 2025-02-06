e5715e4bd674d6acbfbf8cf5039f523ad231fa99
H-Dmytro <d.m.herasymchuk@gmail.com>
2025-02-06 15:04:31 +0200

    HW4

---
+++ Homework_4/src/objects.js
@@ -0,0 +1,12 @@
+const products = {
+    city: 'Kiev',
+    companyName: 'FIxed',
+    address: {
+        street: 'Malushko',
+        building: 1,
+        contactNumber: +380900000000
+    }
+};
+console.log(products);
+console.log(Object.keys(products));
+console.log(Object.values(products));
