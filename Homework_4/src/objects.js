c9781b252be2899903f37997288feeed3df1194f
H-Dmytro <d.m.herasymchuk@gmail.com>
2025-02-07 19:54:42 +0200

    Added some functions

--- Homework_4/src/objects.js
+++ Homework_4/src/objects.js
@@ -5,13 +5,33 @@ const product = {
         street: 'Malushko',
         building: 1,
         contactNumber: +380900000000
+    },
+    func: function () {
+        console.log(this.companyName);
+    },
+    funcTwo(city){
+        console.log('this.city'+ this.city);
+        console.log('param without this = ' + city);
+        this.city=city;
+        this.func();
+    },
+    addPrice (price){
+        this.price=price;
+        return this;
+    },
+    nasted: {
+        prop: 'value',
+        funNasted() {
+            console.log(this.prop);
+        }
     }
 };
 console.log(product);
+product.func();
+product.funcTwo('Test3');
+console.log(product.city);
+console.log(product.nasted.funNasted());
+product.addPrice(100);
+console.log(product);
 console.log(Object.keys(product));
 console.log(Object.values(product));
-
-function addPrice (product, price){
-    return Object.assign(product,{price});
-};
-console.log(addPrice(product, 'Test'));
